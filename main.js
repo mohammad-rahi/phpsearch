69826e6fbe3fb5fcb4ae4649b67a538290c38e8c
mohammad-rahi <mohammadrahi00318@gmail.com>
2022-10-07 11:24:38 +0600

    responsive for all devices

--- main.js
+++ main.js
@@ -8,21 +8,18 @@ const toggleSidebar = (thisElement) => {
     let sidebar = document.querySelector(".navbar_sidebar");
 
     sidebar.classList.toggle("open");
-    document.querySelector(".sidebar_overlay").classList.toggle("open");
+    document.querySelector(".sidebar_overlay").classList.add("open");
     document.querySelector("#container").classList.toggle("open");
-
-    if (sidebar.classList.contains("open")) {
-        thisElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>'
-    }
-    else {
-        thisElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg>'
-    }
+    document.querySelector(".search").classList.add("open");
+    thisElement.classList.toggle("open");
 };
 
 const closeSidebar = (thisElement, bars) => {
     document.querySelector(".navbar_sidebar").classList.remove("open");
     thisElement.classList.remove("open");
     document.querySelector("#container").classList.remove("open");
+    document.querySelector(".search").classList.remove("open");
+    document.querySelector(".sidebar_overlay").classList.remove("open");
 
     bars.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg>'
 };
