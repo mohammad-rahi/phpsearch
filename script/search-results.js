7cdeae4cf68df2f0c59f881265c7ad022e31471a
mohammad-rahi <mohammadrahi00318@gmail.com>
2022-10-08 20:25:09 +0600

    added loacation popup

--- script/search-results.js
+++ script/search-results.js
@@ -38,24 +38,34 @@ const filter = (event) => {
 document.querySelectorAll('.search_filter_handler').forEach((item, i) => {
     item.addEventListener("click", () => {
         document.querySelectorAll('.search_filter_popup')[i].classList.toggle('active');
+        document.querySelector('.search_filter_overlay').classList.add('active');
 
         if (i === 0) {
             document.querySelectorAll('.search_filter_popup')[1].classList.remove('active')
             document.querySelectorAll('.search_filter_popup')[2].classList.remove('active')
+            document.querySelectorAll('.search_filter_popup')[3].classList.remove('active')
         }
         else if (i === 1) {
             document.querySelectorAll('.search_filter_popup')[0].classList.remove('active')
             document.querySelectorAll('.search_filter_popup')[2].classList.remove('active')
+            document.querySelectorAll('.search_filter_popup')[3].classList.remove('active')
+        }
+        else if (i === 2) {
+            document.querySelectorAll('.search_filter_popup')[0].classList.remove('active')
+            document.querySelectorAll('.search_filter_popup')[1].classList.remove('active')
+            document.querySelectorAll('.search_filter_popup')[3].classList.remove('active')
         }
         else {
             document.querySelectorAll('.search_filter_popup')[0].classList.remove('active')
             document.querySelectorAll('.search_filter_popup')[1].classList.remove('active')
+            document.querySelectorAll('.search_filter_popup')[2].classList.remove('active')
         }
 
     });
 
     document.querySelector('.search_filter_overlay').addEventListener('click', () => {
         document.querySelectorAll('.search_filter_popup')[i].classList.remove('active');
+        document.querySelector('.search_filter_overlay').classList.remove('active');
     });
 });
 
