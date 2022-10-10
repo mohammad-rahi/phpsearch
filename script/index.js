fec93ffc62efb3abd8b5c35854b0ebd9e99678e6
mohammad-rahi <mohammadrahi00318@gmail.com>
2022-10-10 12:18:09 +0600

    index.html done

--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
 const search = document.querySelector('#search');
 const suggestions = document.querySelector(".suggestions");
 const sidebarOverlay = document.querySelector('.sidebar_overlay');
+const searchOverlay = document.querySelector(".search_ovelay");
 
 const sidebarOpen = () => {
     sidebar.classList.add("active")
@@ -11,46 +12,6 @@ const sidebarClose = () => {
     sidebar.classList.remove("active")
     sidebarOverlay.classList.remove("active");
 };
-
-const autoComplete = (event) => {
-    let value = event.target.value;
-
-    if (value) {
-        let res = countries.filter(country => {
-            let regx = new RegExp(value, 'gi');
-            return country.match(regx)
-        })
-
-        let list = res.map(country => {
-            return `<li><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class='list_search_icon'><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10 607z" /></svg> ${country}</li>`
-        }).join("");
-
-        suggestions.innerHTML = list;
-
-        search.style.paddingLeft = "5px";
-        document.querySelector(".search_icon_left").style.display = "block";
-        document.querySelector(".search_overlay").style.display = "block";
-        document.querySelector(".close_icon").style.display = "block";
-        suggestions.style.borderTop = "1px solid #efdfdf";
-
-        if (suggestions.scrollHeight) {
-            document.querySelector(".search_group").style.borderRadius = "10px 10px 0 0";
-        }
-        else {
-            document.querySelector(".search_group").style.borderRadius = "10px";
-        }
-
-    }
-    else {
-        suggestions.innerHTML = ""
-        search.style.paddingLeft = "1rem";
-        document.querySelector(".search_icon_left").style.display = "none";
-        document.querySelector(".search_overlay").style.display = "none";
-        document.querySelector(".close_icon").style.display = "none";
-        document.querySelector(".search_group").style.borderRadius = "10px";
-        suggestions.style.borderTop = "none";
-    }
-}
 
 const resetSearch = () => {
     search.value = suggestions.innerHTML = "";
@@ -70,13 +31,73 @@ const searchOverlayHandler = () => {
     suggestions.style.display = 'none'
 }
 
-const suggestionsBLock = () => {
-    suggestions.style.display = "block";
-}
-
 searchOverlay.addEventListener("click", () => {
     suggestions.style.display = "none";
 });
 
-search.addEventListener("input", autoComplete);
-search.addEventListener("focus", suggestionsBLock);
+const checkWindowWidth = () => {
+    if (window.innerWidth < 668) {
+        document.querySelector('.search_form_mobile').classList.add('active')
+        document.querySelector('.search_form_mobile #search').focus();
+        document.querySelector('.search_form_mobile #search').value = search.value;
+    }
+    else {
+        document.querySelector('.search_form_mobile').classList.remove('active')
+
+        const autoComplete = (event) => {
+            let value = event.target.value;
+
+            if (value) {
+                let res = countries.filter(country => {
+                    let regx = new RegExp(value, 'gi');
+                    return country.match(regx)
+                })
+
+                let list = res.map(country => {
+                    return `<li><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class='list_search_icon'><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10 607z" /></svg> ${country}</li>`
+                }).join("");
+
+                suggestions.innerHTML = list;
+
+                search.style.paddingLeft = "5px";
+                document.querySelector(".search_icon_left").style.display = "block";
+                // document.querySelector(".search_overlay").style.display = "block";
+                document.querySelector(".close_icon").style.display = "block";
+                suggestions.style.borderTop = "1px solid #efdfdf";
+
+                if (suggestions.style.display === "none") {
+                    document.querySelector(".search_group").style.borderRadius = "10px 10px 0 0";
+                }
+                else {
+                    document.querySelector(".search_group").style.borderRadius = "10px";
+                }
+
+            }
+            else {
+                suggestions.innerHTML = ""
+                search.style.paddingLeft = "1rem";
+                document.querySelector(".search_icon_left").style.display = "none";
+                // document.querySelector(".search_overlay").style.display = "none";
+                document.querySelector(".close_icon").style.display = "none";
+                document.querySelector(".search_group").style.borderRadius = "10px";
+                suggestions.style.borderTop = "none";
+            }
+        }
+
+        const suggestionsBLock = () => {
+            suggestions.style.display = "block";
+        }
+
+        search.addEventListener("input", autoComplete);
+        search.addEventListener("focus", suggestionsBLock);
+    }
+};
+
+search.addEventListener("focus", checkWindowWidth);
+search.addEventListener("input", checkWindowWidth);
+
+window.addEventListener('resize', () => {
+    if (search.value) {
+        checkWindowWidth();
+    }
+})
---
+++ script/index_mobile.js
@@ -0,0 +1,72 @@
+const searchMobile = document.querySelector('.search_form_mobile #search');
+const suggestionsMobile = document.querySelector(".search_form_mobile .suggestions");
+const searchOverlayMobile = document.querySelector(".search_form_mobile .search_ovelay");
+
+const autoCompleteMobile = (event) => {
+    let value = event.target.value;
+
+    if (value) {
+        let res = countries.filter(country => {
+            let regx = new RegExp(value, 'gi');
+            return country.match(regx)
+        })
+
+        let list = res.map(country => {
+            return `<li><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class='list_search_icon'><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10 607z" /></svg> ${country}</li>`
+        }).join("");
+
+        suggestionsMobile.innerHTML = list;
+
+        searchMobile.style.paddingLeft = "5px";
+        // document.querySelector(".search_overlay").style.display = "block";
+        document.querySelector(".search_form_mobile .close_icon").style.display = "block";
+        suggestionsMobile.style.borderTop = "1px solid #efdfdf";
+
+    }
+    else {
+        suggestionsMobile.innerHTML = ""
+        searchMobile.style.paddingLeft = "1rem";
+        document.querySelector(".search_overlay").style.display = "none";
+        document.querySelector(".search_form_mobile .close_icon").style.display = "none";
+        document.querySelector(".search_group").style.borderRadius = "10px";
+        suggestionsMobile.style.borderTop = "none";
+    }
+}
+
+const resetSearchMobile = () => {
+    searchMobile.value = suggestionsMobile.innerHTML = "";
+    searchMobile.focus();
+
+    searchMobile.style.paddingLeft = "1rem";
+    document.querySelector(".search_icon_left").style.display = "none";
+    document.querySelector(".search_group").style.borderRadius = "10px";
+    suggestionsMobile.style.borderTop = "none";
+}
+
+const searchOverlayHandlerMobile = () => {
+    searchMobile.style.paddingLeft = "1rem";
+    document.querySelector(".search_icon_left").style.display = "none";
+    document.querySelector(".search_overlay").style.display = "none";
+    document.querySelector(".search_group").style.borderRadius = "10px";
+    suggestionsMobile.style.display = 'none'
+}
+
+const searchMobileBack = () => {
+    document.querySelector('.search_form_mobile').classList.remove('active');
+    document.querySelector('#search_form #search').value = '';
+    document.querySelector('#search_form .suggestions').style.display = 'none';
+    document.querySelector('#search_form .close_icon').style.display = 'none';
+    document.querySelector('#search_form .search_icon_left').style.display = 'none';
+}
+
+const suggestionsBLockMobile = () => {
+    suggestionsMobile.style.display = "block";
+}
+
+searchOverlay.addEventListener("click", () => {
+    suggestionsMobile.style.display = "none";
+});
+
+searchMobile.addEventListener("input", autoCompleteMobile);
+
+searchMobile.addEventListener("focus", suggestionsBLockMobile);
